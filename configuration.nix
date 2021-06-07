d80ab03293305540d579fbd688ead5f4d4cfcce1
Pogobanane <aenderboy@gmx.de>
2021-06-07 08:47:17 +0000

    set sudo pw cache timeout

--- configuration.nix
+++ configuration.nix
@@ -84,6 +84,10 @@ with pkgs;
     extraGroups = [ "wheel" ];
   };
 
+  security.sudo.extraConfig = ''
+    Defaults timestamp_timeout=15
+  '';
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   # environment.systemPackages = with pkgs; [
