aa56d96fbb5cbe4237a3afa1268ed99f4b9ea54e
Pogobanane <aenderboy@gmx.de>
2021-06-09 09:10:39 +0000

    adjust firewall and merge with lambda-pirate

--- configuration.nix
+++ configuration.nix
@@ -12,25 +12,13 @@ with pkgs;
     ./modules/nix-daemon.nix
   ];
 
-  # for some reasons our firewall, breaks caligo... no body got time to debug this...
   networking.firewall.enable = true;
-  networking.firewall.allowedTCPPortRanges = [ { from = 0; to = 65535; } ];
-  networking.firewall.allowedUDPPortRanges = [ { from = 0; to = 65535; } ];
-  networking.firewall.checkReversePath = false;
-  #networking.firewall.interfaces."tinc.retiolum".allowedTCPPorts = [ 0 ];
-  # keep in mind that this is unlikely to be effective, because kubernetes
-  # throws iptable rules all over the place
-  networking.firewall.extraCommands = ''
-    iptables -I INPUT 1 -i tinc.retiolum -j DROP
-  '';
-  networking.firewall.extraStopCommands = ''
-    iptables -D INPUT -i tinc.retiolum -j DROP
-  '';
+  networking.firewall.interfaces.enp1s0.allowedTCPPorts = [ 22 ];
 
   programs.sysdig.enable = true;
 
-  services.k3s.enable = true;
-  environment.systemPackages = [ pkgs.k3s ];
+  #services.k3s.enable = true;
+  #environment.systemPackages = [ pkgs.k3s ];
 
   networking.retiolum = {
     ipv4 = "10.243.29.172";
