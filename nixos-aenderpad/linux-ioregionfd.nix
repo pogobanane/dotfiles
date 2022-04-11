55fc2100953bd3266d4ba89f92eb83effda19b5c
Pogobanane <aenderboy@gmx.de>
2022-04-11 13:18:20 +0200

    upgrade to linux 5.16

--- nixos-aenderpad/linux-ioregionfd.nix
+++ nixos-aenderpad/linux-ioregionfd.nix
@@ -1,6 +1,6 @@
 { buildLinux, fetchFromGitHub, pkgs, fetchurl, modDirVersionArg ? null, ... }@args:
 buildLinux (args // rec {
-  version = "5.12.14";
+  version = "5.16.2";
   modDirVersion = if (modDirVersionArg == null) then
     builtins.replaceStrings [ "-" ] [ ".0-" ] version
       else
@@ -8,8 +8,8 @@ buildLinux (args // rec {
   src = fetchFromGitHub {
     owner = "Mic92";
     repo = "linux";
-    rev = "56b6b3611b3a57940a314673e1c7aecbc07976e1";
-    sha256 = "sha256-VKtKBIbUoRGp2xJA7VQvjRGPaTaNP04vrjMXDpmOje8=";
+    rev = "892dbc39579f6305bdc6f0c77c9247599a028d7b";
+    sha256 = "sha256-k/xGMSdFjM4TwzrIsHsXcM+SLCWUVfUh8SOoAYVaCXU=";
   };
 
   kernelPatches = [{
@@ -20,6 +20,6 @@ buildLinux (args // rec {
     '';
   # 5.12 patch list has one fix we already have in our branch
   }]; # ++ pkgs.linuxPackages_5_13.kernel.kernelPatches;
-  extraMeta.branch = "5.12";
+  extraMeta.branch = "5.16";
   ignoreConfigErrors = true;
 } // (args.argsOverride or { }))
