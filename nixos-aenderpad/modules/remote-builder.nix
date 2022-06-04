08cf1f32f64f64ec0a940d95acffeda9fc225798
Pogobanane <aenderboy@gmx.de>
2022-06-04 23:10:20 +0200

    add remote builders

--- nixos-aenderpad/modules/remote-builder.nix
+++ nixos-aenderpad/modules/remote-builder.nix
@@ -4,7 +4,7 @@
     {
       hostName = "ryan.r";
       sshUser = "ssh-ng://nix";
-      sshKey = "/home/peter/.ssh/doctorBuilder";
+      sshKey = "/home/peter/.ssh/doctorBuilder"; # TODO use sops
       system = "x86_64-linux";
       maxJobs = 64;
       supportedFeatures = [
@@ -12,6 +12,26 @@
         "kvm"
         "nixos-test"
       ];
+    }
+    {
+      hostName = "graham.r";
+      sshUser = "ssh-ng://nix";
+      sshKey = "/home/peter/.ssh/doctorBuilder"; # TODO use sops
+      system = "x86_64-linux";
+      maxJobs = 64;
+      supportedFeatures = [
+        "big-parallel"
+        "kvm"
+        "nixos-test"
+      ];
+    }
+    {
+      hostName = "aarch64.nixos.community";
+      maxJobs = 64;
+      sshKey = "/home/peter/.ssh/doctorBuilder";
+      sshUser = "pogobanane";
+      system = "aarch64-linux";
+      supportedFeatures = [ "big-parallel" ];
     }
   ];
 }
