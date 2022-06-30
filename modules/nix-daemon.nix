8e6c8f58be449fe8ec300c674363614217edd87f
Pogobanane <aenderboy@gmx.de>
2022-06-30 10:54:31 +0200

    garnix.io: add binary cache

--- modules/nix-daemon.nix
+++ modules/nix-daemon.nix
@@ -24,11 +24,13 @@
     binaryCaches = [
       "https://nix-community.cachix.org"
       "https://mic92.cachix.org"
+      "https://cache.garnix.io"
     ];
 
     binaryCachePublicKeys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "mic92.cachix.org-1:gi8IhgiT3CYZnJsaW7fxznzTkMUOn1RY4GmXdT/nXYQ="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 
