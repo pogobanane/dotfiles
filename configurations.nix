19463e3e421670eee082baf9a5e001d1c7f6914f
Pogobanane <aenderboy@gmx.de>
2022-06-30 10:47:12 +0200

    sops-nix home-manager: fix

--- configurations.nix
+++ configurations.nix
@@ -9,6 +9,7 @@
       home-manager.nixosModules.home-manager {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs.sops-nix = sops-nix;
         home-manager.users.peter = import ./users-hm/peter.nix;
       }
       {
