6364b4a402820ff3178f166da052cd6cd3998a7a
Pogobanane <aenderboy@gmx.de>
2022-10-12 15:13:29 +0200

    disable remote builders

--- modules/remote-builder.nix
+++ modules/remote-builder.nix
@@ -1,31 +1,43 @@
+# known working: 
+#  - turn on VPN/be at uni
+#  - `sudo nixos-rebuild build` to use a clean ssh config.
+#  - untested: perms on ssh key?
+#  - untested: does nixos cache knowledge about unsuable remotes builders?
 {config, ...}: {
-  nix.distributedBuilds = false;
+  nix.distributedBuilds = true;
+  nix.extraOptions = ''
+    builders-use-substitutes = true
+  '';
   nix.buildMachines = [
+    #{
+    #  speedFactor = 0;
+    #  hostName = "ryan.dse.in.tum.de";
+    #  sshUser = "ssh-ng://nix";
+    #  sshKey = "/home/peter/.ssh/doctorBuilder"; # TODO use sops
+    #  #sshKey = "/tmp/doctorBuilder";
+    #  system = "x86_64-linux";
+    #  maxJobs = 64;
+    #  supportedFeatures = [
+    #    "big-parallel"
+    #    "kvm"
+    #    "nixos-test"
+    #  ];
+    #}
+    #{
+      #speedFactor = 0;
+      #hostName = "graham.dse.in.tum.de";
+      #sshUser = "ssh-ng://nix";
+      #sshKey = "/home/peter/.ssh/doctorBuilder"; # TODO use sops
+      #system = "x86_64-linux";
+      #maxJobs = 64;
+      #supportedFeatures = [
+        #"big-parallel"
+        #"kvm"
+        #"nixos-test"
+      #];
+    #}
     {
-      hostName = "ryan.dse.in.tum.de";
-      sshUser = "ssh-ng://nix";
-      sshKey = "/home/peter/.ssh/doctorBuilder"; # TODO use sops
-      system = "x86_64-linux";
-      maxJobs = 64;
-      supportedFeatures = [
-        "big-parallel"
-        "kvm"
-        "nixos-test"
-      ];
-    }
-    {
-      hostName = "graham.dse.in.tum.de";
-      sshUser = "ssh-ng://nix";
-      sshKey = "/home/peter/.ssh/doctorBuilder"; # TODO use sops
-      system = "x86_64-linux";
-      maxJobs = 64;
-      supportedFeatures = [
-        "big-parallel"
-        "kvm"
-        "nixos-test"
-      ];
-    }
-    {
+      speedFactor = 0;
       hostName = "aarch64.nixos.community";
       maxJobs = 64;
       sshKey = "/home/peter/.ssh/doctorBuilder";
