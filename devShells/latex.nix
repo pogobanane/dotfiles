96664e8b4fd54583d7b3cfcdb2740b5d0f70f2f2
Pogobanane <aenderboy@gmx.de>
2022-10-19 10:18:13 +0200

    devShells: latex

--- devShells/latex.nix
+++ devShells/latex.nix
@@ -2,6 +2,7 @@
   pkgs.mkShell {
     buildInputs = with pkgs; [
       texlab # lang serv
+      biber # needed for biblatex
       tex2nixPkgs.tex2nix
       (texlive.combine { inherit (texlive) scheme-small latexmk latexindent; })
     ];
