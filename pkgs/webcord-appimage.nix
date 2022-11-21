3f4e360b865456f1b8b98a63fe42d5c58592a129
Pogobanane <aenderboy@gmx.de>
2022-11-21 23:22:07 +0100

    webcord: add desktop file and wayland fix

--- pkgs/webcord-appimage.nix
+++ pkgs/webcord-appimage.nix
@@ -1,17 +1,40 @@
-{ lib, appimageTools, fetchurl }:
-appimageTools.wrapType2 {
-  name = "webcord";
+{ lib, appimageTools, fetchurl, writeScript, makeWrapper }:
+appimageTools.wrapType2 rec {
+  pname = "webcord";
+  version = "3.9.3";
+
   src = fetchurl {
     url = "https://github.com/SpacingBat3/WebCord/releases/download/v3.9.3/WebCord-3.9.3-x64.AppImage";
     sha256 = "sha256-CwA5DMECQS3se+xf3wbbS8wLN1ABgrIDan65TocLt4A=";
   };
-  extraPkgs = pkgs: with pkgs; [ ];
+  extraPkgs = pkgs: with pkgs; [ makeWrapper ];
+  #nativeBuildInputs = [ makeWrapper ];
+
+  extraInstallCommands =
+    let contents = appimageTools.extract { inherit pname version src; };
+    #wrapped = writeScript "${pname}" ''
+        #exec $out/bin/${pname}-${version} $\{NIXOS_OZONE_WL:+$\{WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}} $@
+    #'';
+    in ''
+      #cp $\{wrapped} $out/bin/${pname}
+      mv $out/bin/${pname}-${version} $out/bin/${pname}
+      #wrapProgram $out/bin/${pname} \
+      #makeWrapper $out/bin/${pname}-${version} $out/bin/${pname}
+        #--add-flags $\{NIXOS_OZONE_WL:+$\{WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}
+
+      install -m 444 -D ${contents}/WebCord.desktop -t $out/share/applications
+      substituteInPlace $out/share/applications/WebCord.desktop \
+        --replace 'Exec=webcord' 'Exec=${pname} --ozone-platform-hint=auto --enable-features=WaylandWindowDecorations'
+      substituteInPlace $out/share/applications/WebCord.desktop \
+        --replace 'Icon=webcord' "Icon=$out/share/icons/hicolor/512x512/webcord.png"
+      cp -r ${contents}/usr/share/icons $out/share
+    '';
 
   meta = with lib; {
-    description = "A new look into listening and enjoying Apple Music in style and performance.";
-    homepage = "https://github.com/ciderapp/Cider";
-    license = licenses.agpl3;
-    maintainers = [ maintainers.cigrainger ];
+    description = "A Discord and Fosscord client made with the Electron API.";
+    homepage = "A Discord and Fosscord client made with the Electron API.";
+    license = licenses.mit;
+    maintainers = [ maintainers.pogobanane ];
     platforms = [ "x86_64-linux" ];
   };
 }
