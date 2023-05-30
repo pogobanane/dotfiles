d987faa05adb466cf7c65df06e20a6082bb4585c
Pogobanane <aenderboy@gmx.de>
2023-05-31 00:48:44 +0200

    extract flake-packages

---
+++ flake-packages.nix
@@ -0,0 +1,27 @@
+{inputs, lib, ...}: {
+  perSystem = { config, self', inputs', pkgs, system, ... }: {
+    packages = {
+      map-cmd = pkgs.callPackage ./pkgs/map.nix { };
+      loc-git = pkgs.callPackage ./pkgs/loc.nix { inherit (inputs) loc-src; };
+      nixos-generations = pkgs.callPackage ./pkgs/nixos-generations.nix { };
+      self-flake = pkgs.callPackage ./pkgs/self-flake.nix { impure-debug-info = inputs.impurity; };
+      #wluma = pkgs.callPackage ./pkgs/wluma.nix { };
+      #webcord = if "${system}" == "x86_64-linux" then pkgs.callPackage ./pkgs/webcord-appimage.nix { } else null;
+      #webcord = pkgs.callPackage ./pkgs/webcord-appimage.nix { };
+    };
+  };
+  flake = let 
+    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux = {
+      webcord = pkgs.callPackage ./pkgs/webcord-appimage.nix { };
+      cider = pkgs.callPackage pkgs/cider.nix { };
+    };
+
+    #packages.x86_64-linux.geary = pkgs.callPackage pkgs/geary.nix { };
+    #packages.x86_64-linux.geary = pkgs.gnome.geary.overrideAttrs (finalAttrs: previousAttrs: {
+      #mesonFlags = [ "-Dprofile=development" "-Dcontractor=enabled" ];
+      #dontStrip = true;
+    #});
+  };
+}
--- flake.nix
+++ flake.nix
@@ -80,34 +80,14 @@
     { inherit inputs; }
     { 
       imports = [
-        ({...}: {
-          perSystem = {...}: {
-            packages = {
-              #webcord = pkgs.callPackage ./pkgs/webcord-appimage.nix { };
-            };
-          };
-        })
+      ./flake-packages.nix
       ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
         packages = {
-          map-cmd = pkgs.callPackage ./pkgs/map.nix { };
-          loc-git = pkgs.callPackage ./pkgs/loc.nix { inherit (inputs) loc-src; };
-          nixos-generations = pkgs.callPackage ./pkgs/nixos-generations.nix { };
-          self-flake = pkgs.callPackage ./pkgs/self-flake.nix { impure-debug-info = inputs.impurity; };
-          #wluma = pkgs.callPackage ./pkgs/wluma.nix { };
-          #webcord = if "${system}" == "x86_64-linux" then pkgs.callPackage ./pkgs/webcord-appimage.nix { } else null;
-          #webcord = pkgs.callPackage ./pkgs/webcord-appimage.nix { };
         };
       };
       flake = {
-        packages.x86_64-linux.webcord = pkgs.callPackage ./pkgs/webcord-appimage.nix { };
-        packages.x86_64-linux.cider = pkgs.callPackage pkgs/cider.nix { };
-        #packages.x86_64-linux.geary = pkgs.callPackage pkgs/geary.nix { };
-        #packages.x86_64-linux.geary = pkgs.gnome.geary.overrideAttrs (finalAttrs: previousAttrs: {
-          #mesonFlags = [ "-Dprofile=development" "-Dcontractor=enabled" ];
-          #dontStrip = true;
-        #});
         homeConfigurations.peter = inputs.home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
