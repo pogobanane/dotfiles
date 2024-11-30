a22b33ddf955b1d40cf3f6587c1b33bb86a35890
Pogobanane <aenderboy@gmx.de>
2024-11-30 14:51:57 +0100

    autoformat

--- flake-configurations.nix
+++ flake-configurations.nix
@@ -1,4 +1,5 @@
-{ self, inputs, ...}: let
+{ self, inputs, ... }:
+let
   system = "x86_64-linux";
   pkgs = inputs.nixpkgs.legacyPackages.${system};
   flakepkgs = self.packages.${pkgs.hostPlatform.system};
@@ -15,92 +16,94 @@
     inherit self;
   };
   nixosSystem = inputs.nixpkgs.lib.nixosSystem;
-in {
+in
+{
   # TODO: merge this file somehow with the home config as stated in configuration.nix
   systems = [ system ];
   perSystem = { self', pkgs, ... }: {
-      apps = {
-        # today i had to fix stuff using:
-        # sudo mkdir -m 0755 -p /nix/var/nix/{profiles,gcroots}/per-user/$USER
-        # sudo chown -R $USER /nix/var/nix/{profiles,gcroots}/per-user/$USER
-        doctor-home = let
-	  activation-script = pkgs.writeShellScript "activate" ''
-           ${inputs.home-manager.packages.${pkgs.system}.home-manager}/bin/home-manager --option keep-going true --flake "${self}#peter-doctor-cluster" "$@"
-
-	  '';
-	in {
+    apps = {
+      # today i had to fix stuff using:
+      # sudo mkdir -m 0755 -p /nix/var/nix/{profiles,gcroots}/per-user/$USER
+      # sudo chown -R $USER /nix/var/nix/{profiles,gcroots}/per-user/$USER
+      doctor-home =
+        let
+          activation-script = pkgs.writeShellScript "activate" ''
+            ${inputs.home-manager.packages.${pkgs.system}.home-manager}/bin/home-manager --option keep-going true --flake "${self}#peter-doctor-cluster" "$@"
+          '';
+        in
+        {
           type = "app";
           program = "${activation-script}";
         };
-      };
-      #packages = {
-      #  flake-app-doctor-home = pkgs.writeScriptBin "flake-app-doctor-home" ''
-      #    ${self.homeConfigurations.peter-doctor-cluster.activationPackage}/activate
-      #  '';
-      #};
+    };
+    #packages = {
+    #  flake-app-doctor-home = pkgs.writeScriptBin "flake-app-doctor-home" ''
+    #    ${self.homeConfigurations.peter-doctor-cluster.activationPackage}/activate
+    #  '';
+    #};
   };
   flake = {
-        homeConfigurations.peter = inputs.home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            ./homeManager/peter.nix
-          ];
-          extraSpecialArgs = {
-            inherit inputs;
-            inherit flakepkgs;
-            username = "peter";
-            homeDirectory = "/home/peter";
-            my-gui = true;
-          };
-        };
+    homeConfigurations.peter = inputs.home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [
+        ./homeManager/peter.nix
+      ];
+      extraSpecialArgs = {
+        inherit inputs;
+        inherit flakepkgs;
+        username = "peter";
+        homeDirectory = "/home/peter";
+        my-gui = true;
+      };
+    };
 
-        homeConfigurations.peter-doctor-cluster = inputs.home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            ./homeManager/peter.nix
-          ];
-          extraSpecialArgs = {
-            inherit inputs;
-            inherit flakepkgs;
-            username = "okelmann";
-            homeDirectory = "/home/okelmann";
-            my-gui = false;
-          };
-        };
+    homeConfigurations.peter-doctor-cluster = inputs.home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [
+        ./homeManager/peter.nix
+      ];
+      extraSpecialArgs = {
+        inherit inputs;
+        inherit flakepkgs;
+        username = "okelmann";
+        homeDirectory = "/home/okelmann";
+        my-gui = false;
+      };
+    };
 
-        nixosConfigurations.aendernix = nixosSystem {
-          system = "x86_64-linux";
-          modules = extraArgs ++ [
-            ./modules/hardware/hardware-aendernix.nix
-            ./aendernix.nix
-            ./config-common.nix
-            ./modules/gnome.nix
-            ./modules/android-dev.nix
-          ];
-          inherit specialArgs;
-        };
+    nixosConfigurations.aendernix = nixosSystem {
+      system = "x86_64-linux";
+      modules = extraArgs ++ [
+        ./modules/hardware/hardware-aendernix.nix
+        ./aendernix.nix
+        ./config-common.nix
+        ./modules/gnome.nix
+        ./modules/android-dev.nix
+      ];
+      inherit specialArgs;
+    };
 
-        nixosConfigurations.aenderpad = nixosSystem {
-          system = "x86_64-linux";
-          modules = extraArgs ++ [
-            ./modules/hardware/hardware-aenderpad.nix
-            ./aenderpad.nix
-            inputs.nixos-hardware.nixosModules.lenovo-thinkpad-e14-amd
-            ./config-common.nix
-            ./modules/gnome.nix
-          ];
-          inherit specialArgs;
-        };
+    nixosConfigurations.aenderpad = nixosSystem {
+      system = "x86_64-linux";
+      modules = extraArgs ++ [
+        ./modules/hardware/hardware-aenderpad.nix
+        ./aenderpad.nix
+        inputs.nixos-hardware.nixosModules.lenovo-thinkpad-e14-amd
+        ./config-common.nix
+        ./modules/gnome.nix
+      ];
+      inherit specialArgs;
+    };
 
-        # nixosConfigurations.aendernext = nixosSystem {
-        #   system = "x86_64-linux";
-        #   modules = extraArgs ++ [
-        #     ./modules/hardware/hardware-aendernext.nix
-        #     ./aenderpad.nix
-        #     ./config-common.nix
-        #   ];
-        #   inherit specialArgs;
-        # };
+    # nixosConfigurations.aendernext = nixosSystem {
+    #   system = "x86_64-linux";
+    #   modules = extraArgs ++ [
+    #     ./modules/hardware/hardware-aendernext.nix
+    #     ./aenderpad.nix
+    #     ./config-common.nix
+    #   ];
+    #   inherit specialArgs;
+    # };
 
-};
-  }
+  };
+}
