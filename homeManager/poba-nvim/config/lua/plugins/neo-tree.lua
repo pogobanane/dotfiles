3224791325251a5a6e5eec0b09e01fe923450806
Pogobanane <aenderboy@gmx.de>
2025-03-16 17:04:40 +0100

    poba-nvim: neo-tree does not follow files

--- homeManager/poba-nvim/config/lua/plugins/neo-tree.lua
+++ homeManager/poba-nvim/config/lua/plugins/neo-tree.lua
@@ -1,6 +1,17 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
-	opts = {
-		follow_current_file = true,
-	},
+	opts = function (_, opts)
+		-- opts.follow_current_file = false
+		-- opts.filesystems.follow_current_file.enabled = false
+		opts.filesystem.follow_current_file.enabled = false -- this one?!
+		-- opts.filesystem.follow_current_file.leave_dirs_open = true
+		-- opts.sources = { "filesystem" }
+		-- opts.source_selector.sources = { {source = "filesystem"} }
+		-- opts.default_source = "filesystem"
+
+		-- opts.filesystem.bind_to_cwd = false -- creates a 2-way binding between vim's cwd and neo-tree's root
+
+		-- opts.buffers.bind_to_cwb = false
+		-- opts.buffers.follow_current_file.enabled = false -- this one?!
+	end,
 }
