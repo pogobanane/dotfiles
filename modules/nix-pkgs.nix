c6981b7ae19c732c60d39524c48079fbc47ebee0
Pogobanane <aenderboy@gmx.de>
2025-06-16 10:26:55 +0200

    remove garnix cache because its down and blocks rebuilds
    ls -la /etc/nix/nix.conf
    sudo rm /etc/nix/nix.conf
    sudo cp /etc/static/nix/nix.conf /etc/nix/nix.conf
    sudo chmod 644 /etc/nix/nix.conf
    sudo vim /etc/nix/nix.conf # remove garnix from nix.conf to fix builds
    temporarily
    just nixos-switch
    sudo rm /etc/nix/nix.conf
    sudo sln /etc/static/nix/nix.conf /etc/nix/nix.conf

--- modules/nix-pkgs.nix
+++ modules/nix-pkgs.nix
@@ -6,12 +6,10 @@
       substituters = [
         "https://nix-community.cachix.org"
         "https://mic92.cachix.org"
-        "https://cache.garnix.io"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "mic92.cachix.org-1:gi8IhgiT3CYZnJsaW7fxznzTkMUOn1RY4GmXdT/nXYQ="
-        "cache.garnix.io-1:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
     gc.automatic = true;
